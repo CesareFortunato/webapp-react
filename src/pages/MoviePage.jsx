89cb4bace47ae163ae61e40885c8bc7ca639a164
Cesare89 <cf@nume.plus>
2026-02-23 19:47:54 +0100

    feat: card review

--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -1,9 +1,14 @@
 import { Link } from "react-router-dom"
 
+import CardReview from "../components/CardReview"
+
 const MoviePage = () => {
     return (
         <>
             <header id="movie" className="border-bottom border-1 mb-3">
+                <div className="d-flex mb-3">
+                    <img src="https://picsum.photos/300/200" className="card-img-top" alt="titolo_del_film" />
+                </div>
                 <h1>Titolo del film</h1>
                 <h3 className="text-muted"><i>By nome regista</i></h3>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem, ad.
@@ -14,8 +19,12 @@ const MoviePage = () => {
                 <header className="d-flex justify-content-between align-items-center mb-4">
                     <h4>Our community reviews</h4>
                 </header>
-                
-                <p>elenco reviews</p>
+
+                <CardReview />
+                <CardReview />
+                <CardReview />
+                <CardReview />
+                <CardReview />
             </section>
             <footer className="border-top border-1 pt-2 mb-3 d-flex justify-content-end">
                 <Link className="btn btn-secondary" to="/">Back to Home</Link>
