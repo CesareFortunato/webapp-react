1c459fc952c99b7efcfb2a030b58c36a929f64b1
Cesare89 <cf@nume.plus>
2026-02-24 14:27:59 +0100

    feat: setup props on cardmovies + test array with movies

--- src/components/CardMovie.jsx
+++ src/components/CardMovie.jsx
@@ -1,23 +1,23 @@
 import { Link } from "react-router-dom"
 
-const CardMovie = () => {
-return (
+const CardMovie = ({ movie }) => {
+  return (
     <div className="card mb-4">
-        <img src="https://picsum.photos/300/200" className="card-img-top" alt="titolo_del_film" />
-        <div className="card-body">
-            <h5 className="card-title">Titolo film</h5>
-            <address><i>Regista</i></address>
-            <p className="card-text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Voluptatem repellat quam nihil perspiciatis, 
-                deleniti placeat unde earum adipisci impedit 
-                excepturi eligendi soluta deserunt expedita qui aspernatur et ipsam consectetur laudantium.
-            </p>
-            <Link to={`films/2`} className="btn btn-primary">See more</Link>
-        </div>
+      <img src="https://picsum.photos/300/200" className="card-img-top" alt={movie.title} />
+      <div className="card-body">
+        <h5 className="card-title">{movie.title}</h5>
+        <address className="mb-2"><i>{movie.director}</i></address>
 
+        <p className="card-text">
+          {movie.abstract}
+        </p>
+
+        <Link to={`/films/${movie.id}`} className="btn btn-primary">
+          See more
+        </Link>
+      </div>
     </div>
-)
+  )
 }
 
 export default CardMovie
--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -1,36 +1,46 @@
-import { Link } from "react-router-dom"
-
+import { Link, useParams } from "react-router-dom"
 import CardReview from "../components/CardReview"
 
 const MoviePage = () => {
-    return (
-        <>
-            <header id="movie" className="border-bottom border-1 mb-3">
-                <div className="d-flex mb-3">
-                    <img src="https://picsum.photos/300/200" className="card-img-top" alt="titolo_del_film" />
-                </div>
-                <h1>Titolo del film</h1>
-                <h3 className="text-muted"><i>By nome regista</i></h3>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem, ad.
-                    Nostrum libero omnis odit ex praesentium voluptate eius velit quisquam laboriosam!
-                    Culpa quas dolorem temporibus quos ratione quae, numquam corrupti?</p>
-            </header>
-            <section id="reviews">
-                <header className="d-flex justify-content-between align-items-center mb-4">
-                    <h4>Our community reviews</h4>
-                </header>
+  const { id } = useParams()
 
-                <CardReview />
-                <CardReview />
-                <CardReview />
-                <CardReview />
-                <CardReview />
-            </section>
-            <footer className="border-top border-1 pt-2 mb-3 d-flex justify-content-end">
-                <Link className="btn btn-secondary" to="/">Back to Home</Link>
-            </footer>
-        </>
-    )
+  // MOCK (poi diventerà dato dal BE)
+  const movie = {
+    id,
+    title: "Titolo del film",
+    director: "Nome regista",
+    abstract:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem, ad. Nostrum libero omnis odit ex praesentium voluptate eius velit quisquam."
+  }
+
+  return (
+    <>
+      <header id="movie" className="border-bottom border-1 mb-3">
+        <div className="d-flex mb-3">
+          <img src="https://picsum.photos/300/200" className="card-img-top" alt={movie.title} />
+        </div>
+
+        <h1>{movie.title}</h1>
+        <h3 className="text-muted"><i>By {movie.director}</i></h3>
+        <p>{movie.abstract}</p>
+      </header>
+
+      <section id="reviews">
+        <header className="d-flex justify-content-between align-items-center mb-4">
+          <h4>Our community reviews</h4>
+        </header>
+
+        {/* per ora statiche, poi mappiamo un array */}
+        <CardReview />
+        <CardReview />
+        <CardReview />
+      </section>
+
+      <footer className="border-top border-1 pt-2 mb-3 d-flex justify-content-end">
+        <Link className="btn btn-secondary" to="/">Back to Home</Link>
+      </footer>
+    </>
+  )
 }
 
-export default MoviePage 
+export default MoviePage
