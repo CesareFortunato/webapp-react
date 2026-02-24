044c254c6cb8b754b8aa853d85ae6d0fcedbc4b0
Cesare89 <cf@nume.plus>
2026-02-24 16:28:07 +0100

    fix: endpoint in moviePage + feat: dynamic reviews

--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -2,60 +2,40 @@ import { Link, useParams } from "react-router-dom"
 import CardReview from "../components/CardReview"
 import axios from "axios"
 import { useState, useEffect } from "react"
-
-const movie = {
-    "id": 2,
-    "title": "The Godfather",
-    "director": "Francis Ford Coppola",
-    "genre": "Crime",
-    "release_year": 1972,
-    "abstract": "The story of a powerful Italian-American crime family and their struggles.",
-    "image": "interstellar.jpg",
-    "created_at": "2024-11-29T10:40:13.000Z",
-    "updated_at": "2025-05-22T10:55:27.000Z",
-    "reviews": [
-        {
-            "id": 4,
-            "movie_id": 2,
-            "name": "Diana",
-            "vote": 5,
-            "text": "The best crime movie ever made.",
-            "created_at": "2024-11-29T10:40:13.000Z",
-            "updated_at": "2024-11-29T10:40:13.000Z"
-        },
-        {
-            "id": 5,
-            "movie_id": 2,
-            "name": "Eve",
-            "vote": 5,
-            "text": "A cinematic classic that never gets old.",
-            "created_at": "2024-11-29T10:40:13.000Z",
-            "updated_at": "2024-11-29T10:40:13.000Z"
-        },
-        {
-            "id": 6,
-            "movie_id": 2,
-            "name": "Frank",
-            "vote": 4,
-            "text": "A bit slow-paced but very impactful.",
-            "created_at": "2024-11-29T10:40:13.000Z",
-            "updated_at": "2024-11-29T10:40:13.000Z"
-        }
-    ]
-}
+const endpoint = "http://localhost:3000/api/films/"
 
 
 
 const MoviePage = () => {
 
+    //prendiamo id dall url
+    const { id } = useParams();
+
+
+    //settiamo var di stato
+    const [movie, setMovie] = useState({});
+
+
+    const fetchMovie = () => {
+        axios
+            .get(endpoint + id)
+            .then((res) => setMovie(res.data))
+            .catch((err) => console.log(err))
+    }
+
+    useEffect(() => {
+        fetchMovie()
+    }, [])
+
+
     const renderReviews = () => {
-        return movie.reviews.map(review => {
+        return movie.reviews?.map(review => {
             return (
                 <CardReview reviewProp={review} key={review.id} />
             )
         })
     }
-    const { id } = useParams()
+
 
 
 
