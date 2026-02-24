a47701b9f60c1f5acdeb7189859032e7b20657ac
Cesare89 <cf@nume.plus>
2026-02-24 16:09:51 +0100

    fix: route films

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,87 +1,41 @@
 import CardMovie from "../components/CardMovie"
+import axios from "axios"
+import { useState, useEffect } from "react"
 
-const movies = [{
-    "id": 1,
-    "title": "Inception",
-    "director": "Christopher Nolan",
-    "genre": "Science Fiction",
-    "release_year": 2010,
-    "abstract": "A skilled thief is given a chance at redemption if he can successfully perform inception.",
-    "image": "inception.jpg",
-    "created_at": "2024-11-29T10:40:13.000Z",
-    "updated_at": "2025-05-22T10:55:27.000Z"
-},
-{
-    "id": 2,
-    "title": "The Godfather",
-    "director": "Francis Ford Coppola",
-    "genre": "Crime",
-    "release_year": 1972,
-    "abstract": "The story of a powerful Italian-American crime family and their struggles.",
-    "image": "interstellar.jpg",
-    "created_at": "2024-11-29T10:40:13.000Z",
-    "updated_at": "2025-05-22T10:55:27.000Z"
-},
-{
-    "id": 3,
-    "title": "Titanic",
-    "director": "James Cameron",
-    "genre": "Romance",
-    "release_year": 1997,
-    "abstract": "A romantic story set against the tragic sinking of the RMS Titanic.",
-    "image": "matrix.jpg",
-    "created_at": "2024-11-29T10:40:13.000Z",
-    "updated_at": "2025-05-22T10:55:27.000Z"
-},
-{
-    "id": 4,
-    "title": "The Matrix",
-    "director": "The Wachowskis",
-    "genre": "Action",
-    "release_year": 1999,
-    "abstract": "A hacker discovers the truth about his reality and his role in the war against its controllers.",
-    "image": "the_godfather.jpg",
-    "created_at": "2024-11-29T10:40:13.000Z",
-    "updated_at": "2025-05-22T10:55:27.000Z"
-},
-{
-    "id": 5,
-    "title": "Interstellar",
-    "director": "Christopher Nolan",
-    "genre": "Science Fiction",
-    "release_year": 2014,
-    "abstract": "A team of explorers travels through a wormhole in space to save humanity.",
-    "image": "titanic.jpg",
-    "created_at": "2024-11-29T10:40:13.000Z",
-    "updated_at": "2025-05-22T10:55:27.000Z"
-}];
+const endpoint = "http://localhost:3000/api/films"
 
 const HomePage = () => {
+    const [movies, setMovies] = useState([])
 
-    const renderMovies = () => {
-        return movies.map(movie => {
-            return (
-                <div className="col" key={movie.id}>
-                    <CardMovie movieProp={movie}/>
-                </div>
-            )
-        })
+    const fetchMovies = () => {
+        axios
+            .get(endpoint)
+            .then((res) => setMovies(res.data))
+            .catch((err) => console.log(err))
     }
 
+    const renderMovies = () => {
+        return movies.map((movie) => (
+            <div className="col" key={movie.id}>
+                <CardMovie movie={movie} />
+            </div>
+        ))
+    }
+
+    useEffect(() => {
+        fetchMovies()
+    }, [])
+
     return (
-
-
         <>
             <h1 className="text-primary">Boolbuster</h1>
             <h2><i>The boolean nerd movie community</i></h2>
 
-            <div>
-                <div className="row row-cols-3 mt-4">
-                    {renderMovies()}
-                </div>
+            <div className="row row-cols-3 mt-4">
+                {renderMovies()}
             </div>
         </>
     )
 }
 
-export default HomePage 
+export default HomePage
